4a5c0713089d9cdf4e3a791e86c16e1a4bb88f1d
Israel Sanchez <Israel.pro1994@gmail.com>
2021-04-06 14:32:37 -0500

    worked mostly on backend, frontend was just npx

---
+++ muscle_backend/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+// import bodyparser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+mongoose.Promise = global.Promise; 
+mongoose.connect('mongodb:mongodb://127.0.0.1:27017/muscle_data', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Body-Parser via Express Setup 
+app.use(express.json({ extended: false }));
+
+// CORS Setup
+app.use( cors() );
+
+app.get('/', ( req, res ) =>
+    res.send(`The social network for fitness people is on port: ${PORT}`)
+) 
+
+app.listen(PORT, () => 
+    console.log(`Social network working on port: ${PORT}`)
+)
