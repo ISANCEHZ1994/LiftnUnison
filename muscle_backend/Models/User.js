9cbfa371aa5641305f115130739d0434bfc494f5
Israel Sanchez <Israel.pro1994@gmail.com>
2021-04-06 14:47:53 -0500

    setting up user models and user controllers

---
+++ muscle_backend/Models/User.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema({
+    fullName: {
+       type: String,
+       required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        // unique: true, // not avaiable because we need to use npm i mongoose-unique-validator
+    },
+    phone: {
+        type: Number, //   NOTICE NOT INTEGER
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    boi: {
+        type: String,
+    },
+    date: {
+         type: Date, 
+         default: Date.now 
+    },
+
+});
