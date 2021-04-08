372f5f08900963767e2d03e97583d8fc4b11c0bb
Israel Sanchez <Israel.pro1994@gmail.com>
2021-04-08 18:59:17 -0500

    all routes work except for update/put

--- muscle_backend/Routes/UserRoutes.js
+++ muscle_backend/Routes/UserRoutes.js
@@ -10,18 +10,18 @@ import {
 
 const routes = (app) => {
     app.route('/users')
-    // GET endpoint
-    .get(getUsers)
-    // POST endpoint
-    .post(addNewUser);
+        // we can get/see all users
+       .get(getUsers)
+         // can add a new user
+       .post(addNewUser);
 
     app.route('/user/:UserId')
-    // GET Specific Player
-   .get(getUserWithId)
-    // UPDATE Specifc Player
-    .put(updateUser)
-    // DELETE Specifc Player
-    .delete(deleteUser)
+    
+       .get(getUserWithId) // can find/see a specifc user
+   
+       .put(updateUser) // this NEEDS work!!!! - cannot update!
+   
+       .delete(deleteUser) // user can be deleted
 }
 
 export default routes;
